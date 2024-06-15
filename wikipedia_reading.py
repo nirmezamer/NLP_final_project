d95868923c455316254274e72ba085d9b71de8b9
yuvalmiz <yuvalmizrahi@mail.tau.ac.il>
2024-06-15 17:08:19 +0300

    initial commit

--- wikipedia_reading.py
+++ wikipedia_reading.py
@@ -1,13 +1,15 @@
 import wikipediaapi
 import google.generativeai as genai
 import requests
-
+import json
+import numpy as np
+from tqdm import tqdm
 
 genai.configure(api_key="AIzaSyANbFX_VEG9NW3LHr-3xEAyTF5r0F5tNw0")
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-msg = """Please generate a Wikipedia-style summary of a random topic. The summary should be informative, neutral in tone, and written in an encyclopedic style typical of Wikipedia articles. Ensure the summary includes key aspects such as the topic's significance, relevant history, notable features, and important figures. The information should sound credible and include specific details like dates, names, and places. The summary should be a single, well-structured paragraph."""
+msg = """Please generate a Wikipedia-style summary of a random topic. The summary should be informative, neutral in tone, and written in an encyclopedic style typical of Wikipedia articles. Ensure the summary includes key aspects such as the topic's significance, relevant history, notable features, and important figures. The information should sound credible and include specific details like dates, names, and places. The summary should be a single, well-structured paragraph. after all that generate a title to the summary ass well that will be fit to wikipedia-style topic. i want the title to be at the start and will be following with a """
 
 
 def read_wikipedia_page(page_title):
@@ -26,8 +28,8 @@ def read_wikipedia_page(page_title):
 
 def call_prompt(msg):
     prompt = model.generate_content(msg)
-    print(prompt.text)
-    return prompt
+    
+    return prompt.text
 
 
 def get_random_wikipedia_article():
@@ -58,9 +60,23 @@ def get_random_wikipedia_article():
         
         except requests.exceptions.RequestException as e:
             print(f"Error fetching Wikipedia article: {e}")
-    return summary
+    return random_article_title, summary
 
 # call_prompt(msg)
+num_human_summaries = 10
+num_LLM_summaries = 10
+human_data = {}
+for i in tqdm(range(num_human_summaries)):
+    title, summary = get_random_wikipedia_article()
+    human_data[title] = summary
 
-for i in range(10):
-    print(get_random_wikipedia_article(), "\n", "-"*50, "\n")
+with open("humen_wikipedia_summaries.json", "w") as file:
+    json.dump(human_data, file)
+
+
+LLM_data = {}
+for i in tqdm(range(num_LLM_summaries)):
+    prompt = call_prompt(msg)
+    LLM_data[i] = prompt
+with open("LLM_wikipedia_summaries.json", "w") as file:
+    json.dump(LLM_data, file)
