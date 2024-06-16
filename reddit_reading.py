640932d6f547aa0f47eb671a7326f1f134a5491c
nirmezamer <nirmezamer@mail.tau.ac.il>
2024-06-16 18:17:21 +0300

    reddit_reading json generation

--- reddit_reading.py
+++ reddit_reading.py
@@ -1,3 +1,4 @@
+import json
 import praw
 import random
 
@@ -12,14 +13,31 @@ def get_random_reddit_post(subreddit_name):
                          user_agent=USER_AGENT)
 
     subreddit = reddit.subreddit(subreddit_name)
-    posts = list(subreddit.hot(limit=100))  # Fetch the top 100 hot posts
+    
+    num_posts = 10
+    posts = list(subreddit.hot(limit=num_posts)) 
+    
+    posts_dict = {}
+    for post in posts:
+        posts_dict[post.title] = {
+            "title": post.title,
+            "score": post.score,
+            "url": post.url,
+            "content": post.selftext,
+            "num_comments": post.num_comments,
+            "comments": [comment.body for comment in post.comments.list()]
+        }
+    with open("reddit_postd.json", "w") as file:
+        json.dump(posts_dict, file) 
 
-    random_post = random.choice(posts)
+    # random_post = random.choice(posts)
 
-    print(f"Title: {random_post.title}")
-    print(f"Score: {random_post.score}")
-    print(f"URL: {random_post.url}")
-    print(f"Content: {random_post.selftext}")
+    # print(f"Title: {random_post.title}")
+    # print(f"Score: {random_post.score}")
+    # print(f"URL: {random_post.url}")
+    # print(f"Content: {random_post.selftext}")
+    # print(f"Number of comments: {random_post.num_comments}")
+    # print(f"Comments: {random_post.comments.list()[0].body}")
 
 # Example usage
 if __name__ == "__main__":
