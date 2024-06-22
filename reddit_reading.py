449fe885890bc97acb9d6b9fe8c2d542283d484b
nirmezamer <nirmezamer@mail.tau.ac.il>
2024-06-22 12:10:26 +0300

    humanities reddit data set

--- reddit_reading.py
+++ reddit_reading.py
@@ -1,20 +1,59 @@
 import json
 import praw
 import random
+import os
 
 # Replace these with your own credentials
 CLIENT_ID = '39ZflIZLYQso2iFg9GqW2g'
 CLIENT_SECRET = 'OnT4P7VFwU4gP6-Zz4Ke8y42FkcunQ'
 USER_AGENT = 'your_user_agent'
 
-def get_random_reddit_post(subreddit_name):
+def get_subreddits_list():
+    subreddits = [
+    # General Interest
+    "AskReddit", "IAmA", 
+    
+    # Technology
+    "technology", "gadgets", 
+    
+    # Science
+    "science", "space", 
+    
+    # Entertainment
+    "movies", "television", 
+    
+    # Hobbies and Interests
+    "cooking", "fitness", 
+    
+    # Gaming
+    "gaming", "pcgaming", 
+    
+    # Art and Creativity
+    "Art", "Design", 
+    
+    # Humor
+    "funny", "Memes", 
+    
+    # Lifestyle
+    "LifeProTips", "GetMotivated", 
+    
+    # News and Politics
+    "news", "worldnews", 
+    
+    # Miscellaneous
+    "cats", "dogs", 
+    ]
+
+    return subreddits
+
+def gen_single_reddit_json(subreddit_name, file_name):
     reddit = praw.Reddit(client_id=CLIENT_ID,
                          client_secret=CLIENT_SECRET,
                          user_agent=USER_AGENT)
 
     subreddit = reddit.subreddit(subreddit_name)
     
-    num_posts = 10
+    num_posts = 20
     posts = list(subreddit.hot(limit=num_posts)) 
     
     posts_dict = {}
@@ -25,20 +64,42 @@ def get_random_reddit_post(subreddit_name):
             "url": post.url,
             "content": post.selftext,
             "num_comments": post.num_comments,
-            "comments": [comment.body for comment in post.comments.list()]
+            "comments": [comment.body for comment in post.comments.list() if type(comment) == praw.models.Comment]
         }
-    with open("reddit_postd.json", "w") as file:
-        json.dump(posts_dict, file) 
+    with open(file_name, "w") as file:
+        json.dump(posts_dict, file, indent=4)
 
-    # random_post = random.choice(posts)
-
-    # print(f"Title: {random_post.title}")
-    # print(f"Score: {random_post.score}")
-    # print(f"URL: {random_post.url}")
-    # print(f"Content: {random_post.selftext}")
-    # print(f"Number of comments: {random_post.num_comments}")
-    # print(f"Comments: {random_post.comments.list()[0].body}")
+def gen_many_humanities_reddit_json():
+    
+    subreddits = get_subreddits_list()
+    new_dir_path = "./reddit_humanities_data_new"
+    if not os.path.exists(new_dir_path):
+        os.makedirs(new_dir_path)
+    
+    # for subreddit in subreddits:
+    #     gen_single_reddit_json(subreddit, new_dir_path + "/" + subreddit + ".json")
+        
+    dir_path = "./reddit_humanities_data"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    
+    overide = ""
+    while overide != "y" and overide != "n":
+        overide = input("Do you want to override the existing data? (y/n): ")
+    
+    if overide == "y":
+        ## remove the dir in dir_path and replace with new_dir_path in windows
+        os.system("rmdir " + dir_path)
+        os.system("move " + new_dir_path + " " + dir_path)
+        # os.system("rm -rf " + dir_path)
+        # os.system("mv " + new_dir_path + " " + dir_path)
+    
+    print("Data collection complete.")
+    return
+    
 
 # Example usage
 if __name__ == "__main__":
-    get_random_reddit_post('python')  # Replace 'python' with any subreddit name
+    
+    # gen_single_reddit_json('python', "reddit_postd.json")  # Replace 'python' with any subreddit name
+    gen_many_humanities_reddit_json()
