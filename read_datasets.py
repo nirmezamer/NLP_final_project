2a7ac573cb620907d2cc66a6ce62cc6e15080f92
yuvalmiz <yuvalmizrahi@mail.tau.ac.il>
2024-06-29 12:44:28 +0300

    analyzing data and read data

---
+++ read_datasets.py
@@ -0,0 +1,17 @@
+import json
+
+def read_data(data_key_payh):
+    data = []
+    for key in data_key_payh.keys():
+        data_path = data_key_payh[key]
+        with open(data_path, 'r') as file:
+            json_data = json.load(file)
+            if key == "reddit":
+                for item in json_data.values():
+                    data.append((item['human_comment'], 0))
+                    data.append((item['AI_comment'], 1))
+            else:
+                for item in json_data.values():
+                    data.append((item['human_comment'], 0))
+                    data.append((item['AI_comment'], 1))
+    return data
