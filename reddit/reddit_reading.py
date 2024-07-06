45686addfa3111bc1b8a09cdfd98d5ab168ecdd9
Yuvalmor24 <yuvalmor24@gmail.com>
2024-07-06 13:44:19 +0300

    Modified reddit script and uploaded about 6500 data entries

--- reddit/reddit_reading.py
+++ reddit/reddit_reading.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import time
 import re
 
-genai.configure(api_key="AIzaSyA_a9NStJj6XoMDaGXlbz-v35xCQzTlDqA")
+genai.configure(api_key="AIzaSyCndWWxDbmMg99QowJPxeZDfB8LHWm1y7Y")
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 def call_prompt(prompt):
@@ -141,7 +141,7 @@ def get_humanities_reddit_data():
     
     return data
 
-def generate_AI_comment(title, content, human_comments):
+def generate_AI_comment(subreddit, title, content, human_comments):
     comments = "\n".join(human_comments)
     prompt = f"""You will be provided with a Reddit post and a subset of its comments. Your task is to generate a new comment that fits naturally into the conversation. The generated comment should be coherent, contextually appropriate, and human-like. The goal is to create a comment that would make sense if it appeared in the original thread.
 
@@ -158,6 +158,7 @@ Provide a meaningful contribution to the conversation.
 Output only the comment text. Do not include any introductions, explanations, or closing statements.
 
 Reddit Post:
+Subreddit: {subreddit}
 Title: {title}
 Body: {content}
 
@@ -167,33 +168,70 @@ Your Generated Comment:
     """
     try:
         answer = call_prompt(prompt)
-        return answer.text
+        return answer
     except Exception as e:
+        print(f"Skipping {subreddit} - {title} due to AI comment generation failure.\n")
+        print("Error: ", e, "\n\n-----------------------------------\n\n")
         return ""
+
+def load_existing_titles(file_name):
+    s = set()
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            data = json.load(file)
+        for post in data.keys():
+            s.add(data[post]["title"])
+    return s
+
+def load_existing_data(file_name):
+    data = {}
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            data = json.load(file)
+    return data
 
 def generate_reddit_data_set():
     # data_set[<post_title>]["title"|"content"|"human_comment"|"AI_comment"]
-    data_set = {}
+    data_set = load_existing_data("reddit/reddit_data_set.json")
     raw_data = get_humanities_reddit_data()
+
+    existing_titles = load_existing_titles("reddit/reddit_data_set.json")
     
     raw_data_keys = list(raw_data.keys())
+
+    data_counter = 0
+
     for i in tqdm(range(len(raw_data_keys))):
         subreddit = raw_data_keys[i]
         subreddit_data = raw_data[subreddit]
         for j, post in enumerate(subreddit_data.keys()):
-            if subreddit_data[post]["num_comments"] < 10:
+            if subreddit_data[post]["num_comments"] < 5    \
+               or len(subreddit_data[post]["comments"]) < 5:
+                print(f"\nSkipping {subreddit} - {post} due to insufficient comments.\n")
                 continue
-            AI_comment = generate_AI_comment(subreddit_data[post]["title"], \
+            if subreddit_data[post]["title"] in existing_titles:
+                print(f"\nSkipping {subreddit} - {post} due to existing title.\n")
+                continue
+            AI_comment = generate_AI_comment(subreddit, \
+                                             subreddit_data[post]["title"], \
                                              subreddit_data[post]["content"], \
                                              subreddit_data[post]["comments"][1:5])
             if AI_comment == "":
                 continue
+            
+            data_counter += 1
             data_set[post] = {
                 "title": subreddit_data[post]["title"],
                 "content": subreddit_data[post]["content"],
                 "human_comment": subreddit_data[post]["comments"][0],
                 "AI_comment": AI_comment
             }
+
+            existing_titles.add(subreddit_data[post]["title"])
+
+            if data_counter % 10 == 0:
+                print(f"\nData collected: {data_counter}\n")
+
         # Save the data set to a json file when finish a subreddit
         with open("reddit/reddit_data_set.json", "w") as file:
             json.dump(data_set, file, indent=4)
